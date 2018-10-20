9cd900d805e4e2e9cce04278aff6b1716340abcb
jessemonroy650 <jessemonroy650@yahoo.com>
2018-10-20 03:46:44 -0600

    debug

--- txt2Speech.js
+++ txt2Speech.js
@@ -2,6 +2,7 @@ var txt2SpeechPlugin = {
     version : '0.7.0',
 
     talk : function (text) {
+        document.getElementById('status').innerHTML = 'talk() called';
         .speak('hello, world!', function () {
             alert('success');
         }, function (reason) {
