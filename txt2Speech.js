a13e88e60cc151b1dd68dc37de2700a7822271be
jessemonroy650 <jessemonroy650@yahoo.com>
2018-10-20 19:35:23 -0600

    update txt2Speech.js

--- txt2Speech.js
+++ txt2Speech.js
@@ -1,36 +1,30 @@
 var txt2SpeechPlugin = {
     version : '1.0.0',
 
-    talk : function (theText) {
+    gotit : function () {
+        document.getElementById('status').innerHTML = 'talk() DONE.';
+    },
+    err   : function (reason) {
+        document.getElementById('status').innerHTML = reason;
+    },
+    talk  : function (theText) {
         //
-        document.getElementById('status').innerHTML = 'talk() called:' + theText;
+        document.getElementById('status').innerHTML = 'talk() => called';
         if (theText == 1) {
-            TTS.speak('hello, world!',
-            function () {},
-            function (reason) {
-                alert(reason);
-            });
+            TTS.speak('hello, world!', txt2SpeechPlugin.gotit, txt2SpeechPlugin.err);
         } else if (theText == 2) {
             TTS.speak({
                 text: 'hello, world!',
                 locale: 'en-GB',
                 rate: 0.65
-            },
-            function () {},
-            function (reason) {
-                alert(reason);
-            });
+            }, txt2SpeechPlugin.gotit, txt2SpeechPlugin.err);
         } else {
             TTS.speak({
                 text: theText,
                 locale: 'en-US',
                 rate: 0.65
-            },
-            function () {},
-            function (reason) {
-                alert(reason);
-            });
+            }, txt2SpeechPlugin.gotit, txt2SpeechPlugin.err);
         }
+        document.getElementById('status').innerHTML = 'talk() => <b>waiting</b>';
     }
 }
-
